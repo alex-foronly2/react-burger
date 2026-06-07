51b3b0a58b081a00f8ff8648a33ba2861c0eecb8
alex-foronly2 <foronly2@mail.ru>
2026-06-08 00:03:36 +0800

    Исправить опечатку

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 import { AppHeader } from '@components/app-header/app-header';
 
-import styles from './layour.module.css';
+import styles from './layout.module.css';
 
 export const Layout = () => {
   return (
