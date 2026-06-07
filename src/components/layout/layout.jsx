1abd2e68546b5ebf73dbb11b85950f2235771eff
alex-foronly2 <foronly2@mail.ru>
2026-06-08 00:01:46 +0800

    Fix styles

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from 'react-router-dom';
 
 import { AppHeader } from '@components/app-header/app-header';
 
+import styles from './layour.module.css';
+
 export const Layout = () => {
   return (
     <>
-      <div>
+      <div className={styles.app}>
         <AppHeader />
         <Outlet />
       </div>
