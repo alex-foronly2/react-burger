0c399cbf5daf3ca0ef658ae3b49ee4ba71a6090c
alex-foronly2 <foronly2@mail.ru>
2026-06-28 18:56:07 +0800

    Внести правки после ревью

--- src/services/tasks/order.test.ts
+++ src/services/tasks/order.test.ts
@@ -18,37 +18,6 @@ import { orderReducer } from './orderReducer';
 
 import type { ingredientType } from '@services/user/slice.moke';
 
-// type ingredientType = 'main' | 'sauce';
-//
-// export const orderBun = {
-//   _id: '692889f16bf770001bfeb4cd',
-//   name: 'Флюоресцентная булка R2-D3',
-//   type: 'bun' as const,
-//   proteins: 44,
-//   fat: 26,
-//   carbohydrates: 85,
-//   calories: 643,
-//   price: 988,
-//   image: 'https://code.s3.yandex.net/react/code/bun-01.png',
-//   image_mobile: 'https://code.s3.yandex.net/react/code/bun-01-mobile.png',
-//   image_large: 'https://code.s3.yandex.net/react/code/bun-01-large.png',
-//   __v: 0 as const,
-// };
-// export const orderIngredient = {
-//   _id: '692889f16bf770001bfeb4d0',
-//   name: 'Говяжий метеорит (отбивная)',
-//   type: 'main' as ingredientType,
-//   proteins: 800,
-//   fat: 800,
-//   carbohydrates: 300,
-//   calories: 2674,
-//   price: 3000,
-//   image: 'https://code.s3.yandex.net/react/code/meat-04.png',
-//   image_mobile: 'https://code.s3.yandex.net/react/code/meat-04-mobile.png',
-//   image_large: 'https://code.s3.yandex.net/react/code/meat-04-large.png',
-//   __v: 0 as const,
-// };
-
 describe('Order Reducers', () => {
   const bun = {
     uniqueId: 'bun1',
